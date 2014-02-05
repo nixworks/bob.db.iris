99dd4a037e797f3bc87efd6fa0a34911c4aff56a
Andre Anjos <andre.anjos@idiap.ch>
2014-02-05 17:43:45 +0100

    Remove unittest

--- xbob/db/iris/test_scripts.py
+++ xbob/db/iris/test_scripts.py
@@ -1,41 +1,37 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8 :
 # Andre Anjos <andre.anjos@idiap.ch>
-# Tue 21 Aug 2012 13:20:38 CEST 
+# Tue 21 Aug 2012 13:20:38 CEST
 
-"""Tests various scripts for xbob.db
+"""Tests various scripts for our xbob.db driver
 """
 
-import unittest
+def test_iris_files():
 
-class IrisDBScriptTest(unittest.TestCase):
+  from xbob.db.iris.driver import Interface
+  import os
 
-  def test00_iris_files(self):
+  for k in Interface().files():
+    assert os.path.exists(k)
 
-    from xbob.db.iris.driver import Interface
-    import os
+def test_iris_dump():
 
-    for k in Interface().files():
-      self.assertTrue(os.path.exists(k))
+  from xbob.db.base.script.dbmanage import main
+  cmdline = 'iris dump --self-test'
+  assert main(cmdline.split()) == 0
 
-  def test01_iris_dump(self):
-   
-    from xbob.db.base.script.dbmanage import main
-    cmdline = 'iris dump --self-test'
-    self.assertEqual(main(cmdline.split()), 0)
+def test_iris_dump():
 
-  def test02_iris_dump(self):
-    
-    from xbob.db.base.script.dbmanage import main
-    cmdline = 'iris dump --class=versicolor --self-test'
-    self.assertEqual(main(cmdline.split()), 0)
+  from xbob.db.base.script.dbmanage import main
+  cmdline = 'iris dump --class=versicolor --self-test'
+  assert main(cmdline.split()) == 0
 
-  def test03_iris_files(self):
+def test_iris_files():
 
-    from xbob.db.base.script.dbmanage import main
-    self.assertEqual(main('iris files'.split()), 0)
+  from xbob.db.base.script.dbmanage import main
+  assert main('iris files'.split()) == 0
 
-  def test04_iris_version(self):
+def test_iris_version():
 
-    from xbob.db.base.script.dbmanage import main
-    self.assertEqual(main('iris version'.split()), 0)
+  from xbob.db.base.script.dbmanage import main
+  assert main('iris version'.split()) == 0
